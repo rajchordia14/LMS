0986e51b6d79223898817af6fcc8f448a278e3d6
rajchordia14 <19ucs227@lnmiit.ac.in>
2022-07-26 23:37:36 +0530

    setup server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-hy
+var express     = require('express');
+var app         = app();
+var bodyPraser  = require('body-praser');
+var path        = require('path');
+var config      = require('./config');
+
+
+app.use(bodyPraser.urlencoded({extended : true}));
+app.use(bodyPraser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+app.listen(config.port);
+console.log('Listening on port' + config.port);
